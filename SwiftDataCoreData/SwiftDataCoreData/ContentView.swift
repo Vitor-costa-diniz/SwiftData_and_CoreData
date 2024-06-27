31bac5b0b1ae6689cbd431f2c4134f43bfc9edd4
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2024-06-27 16:04:49 -0300

    create project in xcode

---
+++ SwiftDataCoreData/SwiftDataCoreData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataCoreData
+//
+//  Created by Vitor Costa on 27/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataCoreData/SwiftDataCoreData/SwiftDataCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataCoreDataApp.swift
+//  SwiftDataCoreData
+//
+//  Created by Vitor Costa on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
