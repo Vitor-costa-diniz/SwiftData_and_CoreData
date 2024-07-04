5c1d0878261edf9e95499b19c613401aae1eebbc
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2024-07-04 16:54:03 -0300

    set @States for add objetive name

--- SwiftDataCoreData/SwiftDataCoreData/Home/Components/AddObjetiveSheet.swift
+++ SwiftDataCoreData/SwiftDataCoreData/Home/Components/AddObjetiveSheet.swift
@@ -9,16 +9,39 @@ import SwiftUI
 
 struct AddObjetiveSheet: View {
     @Environment(\.dismiss) var dismiss
+    @State var name: String = ""
+    @State var date: Date = Date()
+    @State var notes: String = ""
     
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Hello")
+                TextField("Objetive name", text: $name)
+                    .background(
+                        Rectangle()
+                            .frame(height: 50)
+                            .foregroundStyle(Color.gray.opacity(0.3))
+                            .clipShape(.rect(cornerRadius: 16))
+                    )
+                    .padding(.bottom, 24)
+                
+                TextField("Notes", text: $notes)
+                    .background(
+                        Rectangle()
+                            .frame(height: 50)
+                            .foregroundStyle(Color.gray.opacity(0.3))
+                            .clipShape(.rect(cornerRadius: 16))
+                    )
+                
+                Spacer()
             }
+            .padding(.horizontal)
             .toolbar {
                 ToolbarItem(placement: .cancellationAction) {
                     Button(action: {
                         dismiss()
+                        name = ""
+                        notes = ""
                     }, label: {
                         Text("Cancel")
                             .foregroundStyle(.red)
@@ -33,13 +56,16 @@ struct AddObjetiveSheet: View {
                 ToolbarItem(placement: .primaryAction) {
                     Button(action: {
                         dismiss()
+                        name = ""
+                        notes = ""
                     }, label: {
                         Text("Create")
-                            .foregroundStyle(.blue)
+                            .foregroundStyle(name.isEmpty ? .gray : .blue)
                     })
                 }
             }
         }
+        .presentationDetents([.fraction(0.3)])
     }
     
 }
