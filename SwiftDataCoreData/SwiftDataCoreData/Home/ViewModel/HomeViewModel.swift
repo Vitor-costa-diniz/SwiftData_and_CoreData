01e02d5f2e23018356290789c1dfbef91d70b843
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2024-07-12 01:23:11 -0300

    change to default core data

--- SwiftDataCoreData/SwiftDataCoreData/Home/ViewModel/HomeViewModel.swift
+++ SwiftDataCoreData/SwiftDataCoreData/Home/ViewModel/HomeViewModel.swift
@@ -18,7 +18,7 @@ class HomeViewModel: ObservableObject {
     
     func setContext(_ context: ModelContext) {
         self.modelContext = context
-        self.persistenceService = UserSwiftDataService(modelContext: context)
+        self.persistenceService = UserCoreDataService()
         if let persistence = persistenceService {
             self.user = persistence.fetchUser()
             habits = persistence.fetchAllHabtis() ?? []
