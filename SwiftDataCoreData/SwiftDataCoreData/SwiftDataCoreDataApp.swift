54a0cfe345bfe125b0d3a5ffb2f24fbc51781d89
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2024-07-17 09:25:51 -0300

    Fix problem of autosave using swiftdata

--- SwiftDataCoreData/SwiftDataCoreData/SwiftDataCoreDataApp.swift
+++ SwiftDataCoreData/SwiftDataCoreData/SwiftDataCoreDataApp.swift
@@ -13,7 +13,7 @@ struct SwiftDataCoreDataApp: App {
     var body: some Scene {
         WindowGroup {
             AppTabBar()
-                .modelContainer(for: UserSwiftData.self)
+                .modelContainer(for: UserSwiftData.self, isAutosaveEnabled: false)
         }
     }
 }
